1e92aa3af06706bedb723bda0380f13cd834c09b
Liam Connolly <liam.connolly@ku.edu>
2020-09-09 22:42:20 -0500

    Working on adding labels to coordinates

--- displayTest.py
+++ displayTest.py
@@ -12,7 +12,7 @@ class Display:
         self.font = pygame.font.SysFont("Comic Sans", 16)
 
         boardWidth = self.cell_size * board_size + 2 * margin
-        boardHeight = self.cell_size * 2 * board_size + 3 * margin
+        boardHeight = self.cell_size * 2 * board_size + 3 * margin + 25
 
         self.screen = pygame.display.set_mode((boardWidth, boardHeight)) #initializes screen
         pygame.display.set_caption('Battleship!') #puts caption on top
@@ -20,17 +20,52 @@ class Display:
 
     def screenFill(self):
 
-        for y in range(8):
-            for x in range(8):
-                square = pygame.Rect(50 + x*self.cell_size, y*self.cell_size, self.cell_size, self.cell_size)
-                pygame.draw.rect(self.screen, (255, 255, 255, 255), square, 1)
+
+        xCoordinates = ["A", "B", "C", "D", "E", "F", "G", "H"]
+        yCoordinates = ["8","7","6","5","4","3","2","1", " "]
+
+        font = pygame.font.SysFont('freesansbold.ttf', 50)
+
+        for y in range(9):
+            for x in range(9):
+                if x == 0:
+
+                    square = pygame.Rect(x*self.cell_size, y*self.cell_size, self.cell_size, self.cell_size)
+                    text = font.render(yCoordinates[y], True, (0,0,0))
+                    textRect = text.get_rect()
+                    square.clamp(textRect)
+                    self.screen.blit(text, textRect)
+                    pygame.draw.rect(self.screen, (255, 255, 255, 255), square, 0)
+
+
+
+                elif y == 8:
+                    square = pygame.Rect(x*self.cell_size, y*self.cell_size, self.cell_size, self.cell_size)
+                    pygame.draw.rect(self.screen, (255, 255, 255, 255), square, 0)
+
+                else:
+                    square = pygame.Rect(x*self.cell_size, y*self.cell_size, self.cell_size, self.cell_size)
+                    pygame.draw.rect(self.screen, (255, 255, 255, 255), square, 1)
+
 
 
         buffer = self.margin * 2 + self.board_size * self.cell_size
-        for y in range(8):
-            for x in range(8):
-                square = pygame.Rect(50 + x*self.cell_size, y*self.cell_size + buffer, self.cell_size, self.cell_size)
-                pygame.draw.rect(self.screen, (255, 255, 255, 255), square, 1)
+        for y in range(9):
+            for x in range(9):
+
+                if x == 0:
+                    square = pygame.Rect(x*self.cell_size, y*self.cell_size + buffer, self.cell_size, self.cell_size)
+                    pygame.draw.rect(self.screen, (255, 255, 255, 255), square, 0)
+
+                if y == 8:
+                    square = pygame.Rect(x*self.cell_size, y*self.cell_size + buffer, self.cell_size, self.cell_size)
+                    pygame.draw.rect(self.screen, (255, 255, 255, 255), square, 0)
+
+                else:
+                    square = pygame.Rect(x*self.cell_size, y*self.cell_size + buffer, self.cell_size, self.cell_size)
+                    pygame.draw.rect(self.screen, (255, 255, 255, 255), square, 1)
+
+
 
 
 
